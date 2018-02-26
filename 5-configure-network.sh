16eec946b6e54d4bda3444f674039bc57b8e4790
socket8088 <xavibeltran08@gmail.com>
2018-02-26 09:25:26 +0100

    Update 5-configure-network.sh

--- 5-configure-network.sh
+++ 5-configure-network.sh
@@ -13,8 +13,8 @@ sed -i '10 a \n# START OPENVPN RULES\n# NAT table rules\n*nat\n:POSTROUTING ACCE
 ufw default deny
 #ufw allow from XX.XX.XX.XX to any port 22 # change this and remove the port 22/ssh line that is open to everyone
 #ufw allow 22
-ufw allow 12829
+#ufw allow 12829 #i2p
 ufw allow 1194/udp
 ufw allow from 10.8.0.0/24
 
-#ufw enable
+ufw enable
